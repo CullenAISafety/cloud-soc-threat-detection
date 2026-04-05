8b0412f6130dad9c19259d53d9e815147f15df5f
Cullen Mathews <cullenemathews@gmail.com>
2026-04-05 17:36:32 -0500

    rules.py

--- detection_engine/rules.py
+++ detection_engine/rules.py
@@ -1,1 +1,21 @@
+def detect_bruteforce(events):
 
+    ip_count = {}
+
+    alerts = []
+
+    for event in events:
+
+        ip = event["source_ip"]
+
+        ip_count[ip] = ip_count.get(ip, 0) + 1
+
+        if ip_count[ip] > 10:
+
+            alerts.append({
+                "type": "SSH_BRUTE_FORCE",
+                "source_ip": ip,
+                "attempts": ip_count[ip]
+            })
+
+    return alerts
