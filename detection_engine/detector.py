103b1c01343bac36c777fac7d2575c6a7e4f26da
Cullen Mathews <cullenemathews@gmail.com>
2026-04-05 17:36:02 -0500

    detector.py

--- detection_engine/detector.py
+++ detection_engine/detector.py
@@ -1,1 +1,21 @@
+import re
 
+def parse_auth_log(file_path):
+
+    parsed_logs = []
+
+    with open(file_path, "r") as f:
+        for line in f:
+
+            if "Failed password" in line:
+                match = re.search(r"from (\d+\.\d+\.\d+\.\d+)", line)
+
+                if match:
+                    ip = match.group(1)
+
+                    parsed_logs.append({
+                        "event": "failed_login",
+                        "source_ip": ip
+                    })
+
+    return parsed_logs
