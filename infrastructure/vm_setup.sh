998b813dbfeedb4267afad8d6ae3bf9e905004d6
Cullen Mathews <cullenemathews@gmail.com>
2026-04-05 17:33:58 -0500

    vm_setup.sh

--- infrastructure/vm_setup.sh
+++ infrastructure/vm_setup.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+RESOURCE_GROUP="soc-lab"
+VM_NAME="soc-vm"
+
+az vm create \
+  --resource-group $RESOURCE_GROUP \
+  --name $VM_NAME \
+  --image UbuntuLTS \
+  --admin-username azureuser \
+  --generate-ssh-keys
